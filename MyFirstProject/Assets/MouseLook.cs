b1207ab373a0c66b600993306d2a77132e9ad559
DESKTOP-KI3OO1K\giann <eldjangovive@gmail.com>
2016-09-12 20:51:57 +0200

    Primo commit

---
+++ MyFirstProject/Assets/MouseLook.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensitivityHorizontal = 9.0f;
+    public float sensitivityVertical = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Starting MouseLook Script!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (axes == RotationAxes.MouseX)
+        {
+            // horizontal rotation here
+            Debug.Log("MouseLook Horizontal Rotation");
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHorizontal, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            // vertical rotation here
+            Debug.Log("MouseLook Vertical Rotation");
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVertical;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            transform.Rotate(-(Input.GetAxis("Mouse Y") * sensitivityVertical), 0, 0);
+        }
+        else
+        {
+            // both horizontal and vertical rotation here
+            Debug.Log("MouseLook both Horizontal and Vertical Rotation");
+        }
+    }
+}
---
+++ MyFirstProject/Assets/Spin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public float rotatingSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Spinning Started!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Spinning with speed of " + rotatingSpeed);
+        transform.Rotate(0, rotatingSpeed, 0);
+        //transform.Rotate(rotatingSpeed, 0, 0);
+        //transform.Rotate(0, 0, rotatingSpeed);
+    }
+}
